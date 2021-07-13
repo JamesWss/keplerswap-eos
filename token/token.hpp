9384c377156bcbb98bfb5a9a9bb628cac1d6500e
Stephen N Paris <86105035+StephenNParis@users.noreply.github.com>
2021-07-13 11:00:56 +0800

    Update token.hpp
    Init  token.hpp file.

--- token/token.hpp
+++ token/token.hpp
@@ -9,3 +9,14 @@
 namespace eosiosystem {
    class system_contract;
 }
+
+using namespace eosio;
+using std::string;
+/**
+* eosio.token contract defines the structures and actions that allow users to create, issue, and manage
+* tokens on EOSIO based blockchains.
+*/
+class [[eosio::contract("token")]] token : public contract {
+  public:
+     using contract::contract;
+};
