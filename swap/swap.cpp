c214cca73a275bf1756cbce7fbb07778e7250102
Nancy Raker <68055526+NancyRaker@users.noreply.github.com>
2021-08-11 15:52:26 +0800

    Update swap.cpp
    add void swap::registe(name owner, name inviter) fun.

--- swap/swap.cpp
+++ swap/swap.cpp
@@ -29,3 +29,31 @@ void swap::transfer(name from, name to, asset quantity, std::string memo) {
         });
     }
 }
+
+void swap::registe(name owner, name inviter) {
+    require_auth(owner);
+    whitelist_index whitelist_table(_self, _self.value);
+    auto whitelist_idx = whitelist_table.find(_self.value);
+    if (whitelist_idx != whitelist_table.end()) { //白名单功能打开了,需要判断是否在白名单中
+        whitelist_idx = whitelist_table.find(owner.value);
+        check (whitelist_idx != whitelist_table.end(), "user not avaliable for not in whitelist");
+    }
+    userinfo_index userinfo_table(_self, _self.value);    
+    auto userinfo_idx = userinfo_table.find(owner.value);
+    check (userinfo_idx == userinfo_table.end(), "user already registed");
+    if (userinfo_table.begin() == userinfo_table.end()) {
+        check (inviter == _self, "first user's inviter need to be contract");
+    } else {
+        auto inviter_idx = userinfo_table.find(inviter.value);
+        check (inviter_idx != userinfo_table.end(), "inviter not registed");
+        userinfo_table.modify(inviter_idx, _self, [&](auto& row) {
+            row.num += 1;
+        });
+    }
+    userinfo_table.emplace(_self, [&](auto& row) {
+        row.owner = owner;
+        row.inviter = inviter;
+        row.num = 0;
+        row.create_at = publication_time();
+    });
+}
